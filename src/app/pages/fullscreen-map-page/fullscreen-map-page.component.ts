9b59b1340345df0c5e16a606afe7711b5d2dad61
Acxel Morales <acxelmorales97@gmail.com>
2025-11-25 21:59:47 -0600

    Se agrego mapa en la aplicacion

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
   templateUrl: './fullscreen-map-page.component.html',
+  styles: `
+    div {
+      width: 100vw;
+      height: calc(100vh - 64px);
+    }
+  `
 })
-export class FullscreenMapPageComponent implements OnInit {
+export class FullscreenMapPageComponent implements AfterViewInit {
 
-  constructor() { }
+  divElement = viewChild<ElementRef>('mapa');
+  private map?: mapboxgl.Map;
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    if (!this.divElement()) return;
+
+    setTimeout(() => {
+      const element = this.divElement()?.nativeElement;
+      if (!element) return;
+
+      this.map = new mapboxgl.Map({
+        container: element,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [-74.5, 40],
+        zoom: 9,
+      });
+    }, 80);
   }
 
 }
