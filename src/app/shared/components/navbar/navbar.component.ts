b3803e78725481628040803422308369bfb54a5e
Acxel Morales <acxelmorales97@gmail.com>
2025-11-25 18:13:44 -0600

    Se agrego mejor en el navbar y se creo automatizacion de scripts

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+
+import { filter, map } from 'rxjs';
+
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  imports: [AsyncPipe, RouterLink]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  router = inject(Router);
 
-  ngOnInit() {
-  }
+  routes = routes.map((route) => ({
+    path: route.path,
+    title: route.title ?? 'Mapas en Angular'
+  })).filter(route => route.path !== '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map((event) => event.url),
+    map((url) => routes.find(route => `/${route.path}` === url)?.title ?? 'Mapas')
+  );
 
 }
